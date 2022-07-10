6127e048b76a80b862991970df7068bcfeecd5f6
Oscar Esteban Chinchilla Cruz <65373198+oschcz@users.noreply.github.com>
2022-07-09 22:02:53 -0500

    config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,16 @@
 import theme from "@nuxt/content-theme-docs"
 
 export default theme({
-  docs: {
-    primaryColor: "#E24F55",
+  i18n: {
+    components: [
+      "~/components", // default level is 0
+      {
+        path: "node_modules/@nuxt/content-theme-docs/src/components/app",
+        level: 1,
+      },
+    ],
+    docs: {
+      primaryColor: "#ff5517",
+    },
   },
 })
